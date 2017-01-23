6b27d7eb6d30ef0f80ef3c0d966abb0467ef0375
Michal Jeřábek <mjerabek@seznam.cz>
2017-01-23 14:40:41 +0100

    Styly používají GoogleMapStyle. Grayscale styl odsaturovává výchozí styly.

--- GoogleMap.js
+++ GoogleMap.js
@@ -116,6 +116,15 @@
             GoogleMaps.addMap(this);
         };
 
+    GoogleMap.DEFAULTS = DEFAUlTS;
+
+    GoogleMap.STYLES = {};
+
+    GoogleMap.STYLES.empty = function () {
+
+        return [];
+    };
+
     GoogleMap.prototype.init = function () {
 
         this.$el = this.options.el.jquery ? this.options.el : $(this.options.el);
@@ -123,9 +132,17 @@
 
         var mapOptions = {
             zoom: this.options.zoom,
-            styles: this.options.styles,
             location: new google.maps.LatLng(this.options.coords[0], this.options.coords[1])
         };
+
+        if (this.options.styles instanceof window.GoogleMapStyle) {
+
+            mapOptions.styles = this.options.styles.getStyles();
+
+        } else {
+
+            mapOptions.styles = this.options.styles;
+        }
 
         mapOptions.center = mapOptions.location;
 
@@ -255,25 +272,291 @@
         return info;
     };
 
-    GoogleMap.DEFAULTS = DEFAUlTS;
+    GoogleMap.getStyles = function (name, modifier) {
 
-    GoogleMap.STYLES = {};
-
-    GoogleMap.STYLE = function (name /*, [modifier]*/) {
+        var style;
 
         if (!GoogleMap.STYLES[name]) {
 
-            return [];
+            style = new GoogleMapStyle(GoogleMap.STYLES.empty());
+
+        } else {
+
+            style = new GoogleMapStyle(GoogleMap.STYLES[name]());
         }
 
-        var style = GoogleMap.STYLES[name]();
+        if (typeof modifier === "function") {
 
-        if (arguments[1]) {
-
-            arguments[1](style);
+            modifier.call(style);
         }
 
         return style;
+    };
+
+
+    var GoogleMapStyle = window.GoogleMapStyle = function GoogleMapStyle(styles) {
+
+        this.styles = styles || [];
+    };
+
+    GoogleMapStyle.prototype.set = function (featureType, elementType, styles, unsetSubtypes) {
+
+        var styleFound = false;
+
+        this.each(function (style) {
+
+            if ((style.featureType === featureType || (!style.featureType && !featureType)) && (style.elementType === elementType || (!style.elementType && !elementType))) {
+
+                style.stylers = this.toStylers(styles);
+
+                styleFound = true;
+
+                return false;
+            }
+        });
+
+        if (!styleFound) {
+
+            this.styles.push({
+                featureType: featureType,
+                elementType: elementType,
+                stylers: this.toStylers(styles)
+            });
+        }
+
+        if (unsetSubtypes) {
+
+            this.findSubtypes(featureType, elementType).forEach(function (style) {
+
+                this.unset(style.featureType, style.elementType);
+
+            }.bind(this));
+        }
+    };
+
+    GoogleMapStyle.prototype.all = function (featureType, elementType, styles, unsetSubtypes) {
+
+        this.each(function (style) {
+
+            if ((style.featureType === featureType || !featureType || (style.featureType && style.featureType.indexOf(featureType + ".") === 0)) && (style.elementType === elementType || (!style.elementType && !elementType))) {
+
+                style.stylers = this.toStylers(styles);
+            }
+        });
+
+        if (unsetSubtypes) {
+
+            this.findSubtypes(featureType, elementType).forEach(function (style) {
+
+                this.unset(style.featureType, style.elementType);
+
+            }.bind(this));
+        }
+    };
+
+    GoogleMapStyle.prototype.unset = function (featureType, elementType, subtypes) {
+
+        this.each(function (style, i) {
+
+            if ((style.featureType === featureType || (!style.featureType && !featureType)) && (style.elementType === elementType || (!style.elementType && !elementType))) {
+
+                this.styles.splice(i, 1);
+
+                return false;
+            }
+        });
+
+        if (subtypes) {
+
+            this.findSubtypes(featureType, elementType).forEach(function (style) {
+
+                this.unset(style.featureType, style.elementType);
+
+            }.bind(this));
+        }
+    };
+
+    GoogleMapStyle.prototype.add = function (featureType, elementType, styles, removeSubtypes) {
+
+        var styleFound = false;
+
+        this.each(function (style) {
+
+            if ((style.featureType === featureType || (!style.featureType && !featureType)) && (style.elementType === elementType || (!style.elementType && !elementType))) {
+
+                $.each(styles, function (name, newValue) {
+
+                    var found = false;
+
+                    $.each(style.stylers, function (i, styler) {
+
+                        if (typeof styler[name] !== "undefined") {
+
+                            style.stylers[i][name] = newValue;
+
+                            found = true;
+
+                            return false;
+                        }
+                    });
+
+                    if (!found) {
+
+                        var newStyler = {};
+
+                        newStyler[name] = newValue;
+
+                        style.stylers = style.stylers || [];
+
+                        style.stylers.push(newStyler);
+                    }
+                });
+
+                styleFound = true;
+
+                return false;
+            }
+        });
+
+        if (!styleFound) {
+
+            this.set(featureType, elementType, styles);
+        }
+
+        if (removeSubtypes) {
+
+            this.findSubtypes(featureType, elementType).forEach(function (style) {
+
+                this.remove(style.featureType, style.elementType, styles);
+
+            }.bind(this));
+        }
+    };
+
+    GoogleMapStyle.prototype.remove = function (featureType, elementType, styles, subtypes) {
+
+        styles = typeof styles === "string" ? [styles] : styles;
+
+        var styleFound = false;
+
+        this.each(function (style) {
+
+            if ((style.featureType === featureType || (!style.featureType && !featureType)) && (style.elementType === elementType || (!style.elementType && !elementType))) {
+
+                styles.forEach(function (name) {
+
+                    $.each(style.stylers, function (i, styler) {
+
+                        if (typeof styler[name] !== "undefined") {
+
+                            style.stylers.splice(i, 1);
+
+                            return false;
+                        }
+                    });
+                }.bind(this));
+
+                if (!style.stylers.length) {
+
+                    this.unset(featureType, elementType);
+                }
+
+                styleFound = true;
+
+                return false;
+            }
+        });
+
+        if (!styleFound) {
+
+            this.unset(featureType, elementType, subtypes);
+        }
+
+        if (subtypes) {
+
+            this.findSubtypes(featureType, elementType).forEach(function (style) {
+
+                this.remove(style.featureType, style.elementType, styles);
+
+            }.bind(this));
+        }
+    };
+
+    GoogleMapStyle.prototype.toStylers = function (styles) {
+
+        var stylers = [];
+
+        $.each(styles, function (name, value) {
+
+            var styler = {};
+
+            styler[name] = value;
+
+            stylers.push(styler);
+        });
+
+        return stylers;
+    };
+
+    GoogleMapStyle.prototype.findSubtypes = function (featureType, elementType) {
+
+        var substyles = [],
+
+            featureTypeDot = featureType ? featureType + "." : null,
+            elementTypeDot = elementType ? elementType + "." : null;
+
+        this.each(function (style) {
+
+            var added = false,
+
+                styleHasFeature = !!style.featureType,
+                styleHasElement = !!style.elementType,
+                styleIsFeature = style.featureType === featureType,
+                styleIsElement = style.elementType === elementType,
+                styleIsFeatureSubtype = styleHasFeature && (style.featureType.indexOf(featureTypeDot) === 0 || (featureType === "all" && style.featureType !== "all")),
+                styleIsElementSubtype = styleHasElement && (style.elementType.indexOf(elementTypeDot) === 0 || (elementType === "all" && style.elementType !== "all"));
+
+            if ((!styleHasFeature && !featureType) || (styleHasFeature && (styleIsFeature || styleIsFeatureSubtype))) {
+
+                if (styleHasElement && styleIsElementSubtype) {
+
+                    added = substyles.push(style);
+                }
+            }
+
+            if (!added && (!elementType && (!styleHasElement || (styleHasFeature && styleIsFeatureSubtype)))) {
+
+                if (styleHasFeature && styleIsFeatureSubtype) {
+
+                    added = substyles.push(style);
+                }
+            }
+
+            if (!added && styleHasFeature && styleHasElement) {
+
+                if (((styleIsFeature && styleIsElementSubtype) || (styleIsFeatureSubtype && styleIsElement)) || (styleIsElementSubtype && styleIsFeatureSubtype)) {
+
+                    added = substyles.push(style);
+                }
+
+                if (!added && styleIsFeature && !elementType && styleHasElement) {
+
+                    added = substyles.push(style);
+                }
+            }
+        });
+
+        return substyles;
+    };
+
+    GoogleMapStyle.prototype.each = function (fn) {
+
+        this.getStyles().forEach(fn.bind(this));
+    };
+
+    GoogleMapStyle.prototype.getStyles = function () {
+
+        return this.styles;
     };
 
 }(jQuery));
